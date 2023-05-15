1a20538962ba267e7e0d9f50c0b1ea8a325c7548
Varun-98-masai <mohit.mohit979@gmail.com>
2023-05-15 11:26:02 +0530

    keeper app

---
+++ keeper-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './component/Footer';
+import Header from "./component/Header"
+import Note from './component/Note';
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ keeper-app/src/component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./footer.css"
+const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+  return (
+    <div className='footer'>
+        <p>
+            Copyright @ {year}
+        </p>
+    </div>
+  )
+}
+
+export default Footer
