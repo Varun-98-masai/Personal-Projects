39b950c1b1113c010578f5daa5efb89f0324d54d
Varun-98-masai <mohit.mohit979@gmail.com>
2023-05-17 10:12:44 +0530

    added

--- Udemy/emoji-list/src/App.js
+++ Udemy/emoji-list/src/App.js
@@ -1,30 +1,36 @@
-import './App.css';
-import Entry from './component/Entry';
-import emojidata from './emojidata';
+import "./App.css";
+import Entry from "./component/Entry";
+import emojidata from "./emojidata";
 console.log(emojidata);
-const newEmojiPedia = emojidata.map(function(emojiEntry){
-  return emojiEntry.detail.substring(0, 50)
-})
+const newEmojiPedia = emojidata.map(function (emojiEntry) {
+  return emojiEntry.detail.substring(0, 50);
+});
 console.log(newEmojiPedia);
-function createEntry(emoji) {
- return <Entry
-  key={emoji.id}
-  img={emoji.emoji}
-  title={emoji.title}
-  detail={emoji.detail}
-/>
-}
+// const createEntry = (emoji) => {
+//  return <Entry
+//   key={emoji.id}
+//   img={emoji.emoji}
+//   title={emoji.title}
+//   detail={emoji.detail}
+// />
+// }
 function App() {
   return (
     <div className="App">
-     <h1>
-      <span className='heading'>emojipedia</span>
-     </h1>
+      <h1>
+        <span className="heading">emojipedia</span>
+      </h1>
 
-     <div className='dictionary'>
-      {emojidata.map(createEntry) }
-      
-     </div>
+      <div className="dictionary">
+        {emojidata.map((emoji) => (
+          <Entry
+            key={emoji.id}
+            img={emoji.emoji}
+            title={emoji.title}
+            detail={emoji.detail}
+          />
+        ))}
+      </div>
     </div>
   );
 }
